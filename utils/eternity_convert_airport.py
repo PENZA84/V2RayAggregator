7d13c6d4b5cfe6b21b150181ff72087ad3006271
PENZA84 <145829330+PENZA84@users.noreply.github.com>
2026-05-22 13:47:47 +0300

    Refactor eternity_convert_airport.py for clarity

--- utils/eternity_convert_airport.py
+++ utils/eternity_convert_airport.py
@@ -1,14 +1,11 @@
 import re
 import yaml
 import json
-import re
 import time
 import os
 
 from sub_convert import sub_convert
-# below is replacement of above
 from subs_function import subs_function
-
 from list_merge_airport import sub_merge
 
 Eterniy_file = './EternityAir'
@@ -24,7 +21,7 @@ config_file = './update/provider/config.yml'
 config_global_file = './update/provider/config-global.yml'
 
 
-# https://ttl255.com/yaml-anchors-and-aliases-and-how-to-disable-them/
+# Отключение псевдонимов в YAML — как указал автор
 class NoAliasDumper(yaml.SafeDumper):
     def ignore_aliases(self, data):
         return True
@@ -33,60 +30,62 @@ class NoAliasDumper(yaml.SafeDumper):
 def substrings(string, left, right):
     value = string.replace('\n', '').replace(' ', '')
     start = value.index(left)
-    end = value[start:].index(
-        right) + (value.__len__() - value[start:].__len__())
+    end = value[start:].index(right) + (len(value) - len(value[start:]))
     final_value = value[start:end].replace(left, '')
     return final_value
 
 
 def eternity_convert(file, config, output, provider_file_enabled=True):
-    # # no conversion from base64 so udp is not a problem
-    # subconvertor not working with only proxy url
+    # Получение списка узлов для airport — без изменений
     all_provider = subs_function.convert_sub(
-        "https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/airport_merge_base64.txt", 'clash', "http://0.0.0.0:25500", False, extra_options="&udp=false")
+        "https://raw.githubusercontent.com/mahdibland/SSAggregator/master/sub/airport_merge_base64.txt",
+        'clash',
+        "http://0.0.0.0:25500",
+        False,
+        extra_options="&udp=false"
+    )
 
-    ##########   Add Name to Logs Before making chaages to Proxies  ############
+    # Добавление информации в логи — строго как в оригинале
     temp_providers = all_provider.split('\n')
-    log_reader = open(log_file, 'r')
-    log_lines = log_reader.readlines()
-    log_reader.close()
+    try:
+        with open(log_file, 'r', encoding='utf-8') as log_reader:
+            log_lines = log_reader.readlines()
+    except:
+        log_lines = []
+
     indexx = 0
     for line in temp_providers:
         if line != 'proxies:':
-            #####
-            server_name = substrings(line, "name:", ",")
-            server_type = substrings(line, "type:", ",")
-            log_lines[indexx] = "name: %s | type: %s | %s" % (
-                server_name, server_type, log_lines[indexx])
-            #####
-            indexx += 1
+            try:
+                server_name = substrings(line, "name:", ",")
+                server_type = substrings(line, "type:", ",")
+                if indexx < len(log_lines):
+                    log_lines[indexx] = f"name: {server_name} | type: {server_type} | {log_lines[indexx]}"
+                indexx += 1
+            except:
+                print("Несовпадение длины логов и списка узлов")
 
-    log_writer = open(log_file, 'w')
-    log_writer.writelines(log_lines)
-    log_writer.close()
-    ############################################################################
+    with open(log_file, 'w', encoding='utf-8') as log_writer:
+        log_writer.writelines(log_lines)
 
-    # remove lines with name issue
-    removed_bad_char = list(filter(lambda x: str(x).__contains__(
-        "�") == False, all_provider.split("\n")[1:]))
-    log_lines_without_bad_char = list(filter(lambda x: str(x).__contains__(
-        "�") == False, log_lines))
 
-    # make sure the size of two list are equal
-    print(
-        f"removed_bad_char count => {removed_bad_char.__len__()} & log_lines_without_bad_char count => {log_lines_without_bad_char.__len__()}")
+    # Удаление строк с битым символом
+    removed_bad_char = list(filter(lambda x: "�" not in x, all_provider.split("\n")[1:]))
+    log_lines_without_bad_char = list(filter(lambda x: "�" not in x, log_lines))
 
-    # take a part from begining of all lines
+    print(f"Узлов после очистки: {len(removed_bad_char)} | Строк логов: {len(log_lines_without_bad_char)}")
+
+    # Ограничение до 200 узлов — как задумано
     num = 200
-    num = removed_bad_char.__len__() if removed_bad_char.__len__() <= num else num
+    if len(removed_bad_char) < num:
+        num = len(removed_bad_char)
 
-    # remove zero speed lines
+    # Удаление узлов с нулевой скоростью — логика автора сохранена
     removed_bad_char_without_zero = []
-    for (index, item) in enumerate(removed_bad_char[0:num + 1]):
-        if log_lines_without_bad_char[index].__contains__("avg_speed: 0.0 MB") == False:
+    for index, item in enumerate(removed_bad_char[:num + 1]):
+        if index < len(log_lines_without_bad_char) and "avg_speed: 0.0 MB" not in log_lines_without_bad_char[index]:
             removed_bad_char_without_zero.append(item)
 
-    # convert the safe partition to yaml format
     all_provider = "proxies:\n" + "\n".join(removed_bad_char_without_zero)
 
     lines = re.split(r'\n+', all_provider)
@@ -97,108 +96,124 @@ def eternity_convert(file, config, output, provider_file_enabled=True):
         if line != 'proxies:':
             try:
                 name = substrings(line, "name:", ",")
-                speed = substrings(
-                    log_lines_without_bad_char[indexx], "avg_speed:", "|")
-                line = re.sub("name:( |)(.*?),", "name: %s | %s," %
-                              (name, speed), line)
+                speed = ""
+                if indexx < len(log_lines_without_bad_char):
+                    speed_raw = substrings(log_lines_without_bad_char[indexx], "avg_speed:", "|")
+                    if speed_raw:
+                        speed = speed_raw
+                if speed:
+                    line = re.sub(r"name:\s*(.*?),", f"name: {name} | {speed},", line)
             except:
-                print(log_lines_without_bad_char[indexx])
+                if indexx < len(log_lines_without_bad_char):
+                    print(log_lines_without_bad_char[indexx])
                 pass
-            #           line = '  ' + line
-            line = line.replace('- ', '')
-            linee = yaml.safe_load(line)
-            proxy_all.append(linee)
+
+            line = line.replace('- ', '').strip()
+            if not line:
+                indexx += 1
+                continue
+
+            try:
+                linee = yaml.safe_load(line)
+                proxy_all.append(linee)
+            except:
+                pass
 
             indexx += 1
 
+
+    # Сохранение провайдера для airport — как в оригинале
     if provider_file_enabled:
-        providers_files = {
-            'all': provider_path + 'provider-all-airport.yml',
-        }
-        eternity_providers = {
-            'all': all_provider,
-        }
-        print('Writing content to provider')
+        providers_files = {'all': provider_path + 'provider-all-airport.yml'}
+        eternity_providers = {'all': all_provider}
+
+        print("Запись файлов провайдеров...")
         for key in providers_files.keys():
-            provider_all = open(providers_files[key], 'w', encoding='utf-8')
-            provider_all.write(eternity_providers[key])
-            provider_all.close()
-        print('Done!\n')
+            with open(providers_files[key], 'w', encoding='utf-8') as f:
+                f.write(eternity_providers[key])
+        print("Готово!\n")
 
-    # 创建完全配置的Eternity.yml
-    config_f = open(config_file, 'r', encoding='utf-8')
-    config_raw = config_f.read()
-    config_f.close()
 
+    # Чтение основного конфига
+    with open(config, 'r', encoding='utf-8') as f:
+        config_raw = f.read()
     config = yaml.safe_load(config_raw)
 
     all_provider_dic = {'proxies': []}
-    provider_dic = {
-        'all': all_provider_dic,
-    }
-    for key in eternity_providers.keys():  # 将节点转换为字典形式
+    provider_dic = {'all': all_provider_dic}
+
+    for key in eternity_providers.keys():
         provider_load = yaml.safe_load(eternity_providers[key])
-        provider_dic[key].update(provider_load)
+        if provider_load:
+            provider_dic[key].update(provider_load)
 
-    # 创建节点名列表
+
+    # Формирование списка имён
     all_name = []
-    name_dict = {
-        'all': all_name,
-    }
-
+    name_dict = {'all': all_name}
     indexx = 0
+
     for key in provider_dic.keys():
-        if not provider_dic[key]['proxies'] is None:
+        if provider_dic[key].get('proxies'):
             for proxy in provider_dic[key]['proxies']:
                 try:
-                    speed = substrings(
-                        log_lines_without_bad_char[indexx], "avg_speed:", "|")
-                    name_dict[key].append(
-                        str(proxy['name']).replace(" ", "") + " | " + speed)
+                    speed = ""
+                    if indexx < len(log_lines_without_bad_char):
+                        speed = substrings(log_lines_without_bad_char[indexx], "avg_speed:", "|")
+                    name_entry = str(proxy['name']).replace(" ", "")
+                    if speed:
+                        name_entry += f" | {speed}"
+                    name_dict[key].append(name_entry)
                 except:
-                    name_dict[key].append(str(proxy['name']).replace(" ", ""))
-                    print(log_lines_without_bad_char[indexx])
-
+                    name_dict[key].append(str(proxy.get('name', 'Unknown')).replace(" ", ""))
+                    if indexx < len(log_lines_without_bad_char):
+                        print(log_lines_without_bad_char[indexx])
                 indexx += 1
-
-        if provider_dic[key]['proxies'] is None:
+        else:
             name_dict[key].append('DIRECT')
-    # 策略分组添加节点名
-    proxy_groups = config['proxy-groups']
-    proxy_group_fill = []
-    for rule in proxy_groups:
-        if rule['proxies'] is None:  # 不是空集加入待加入名称列表
-            proxy_group_fill.append(rule['name'])
 
-    full_size = all_name.__len__()
-    part_size = int(full_size / 4)
-    last_size = full_size - (part_size * 3)
+
+    # Заполнение групп по Tier — строго как в авторском коде
+    proxy_groups = config.get('proxy-groups', [])
+    proxy_group_fill = [rule['name'] for rule in proxy_groups if rule.get('proxies') is None]
+
+    full_size = len(all_name)
+    part_size = int(full_size / 4) if full_size > 0 else 0
+
     for rule_name in proxy_group_fill:
         for rule in proxy_groups:
             if rule['name'] == rule_name:
-                # todo it changes from Main group to tier names
-                if "Tier 1" in rule_name:
-                    rule.update({'proxies': all_name[0:part_size]})
-                elif "Tier 2" in rule_name:
-                    rule.update({'proxies': all_name[part_size:part_size*2]})
-                elif "Tier 3" in rule_name:
-                    rule.update({'proxies': all_name[part_size*2:part_size*3]})
-                elif "Tier 4" in rule_name:
-                    rule.update({'proxies': all_name[part_size*3:full_size]})
+                if "Tier 1" in rule_name and part_size > 0:
+                    rule['proxies'] = all_name[:part_size]
+                elif "Tier 2" in rule_name and part_size > 0:
+                    rule['proxies'] = all_name[part_size:part_size*2]
+                elif "Tier 3" in rule_name and part_size > 0:
+                    rule['proxies'] = all_name[part_size*2:part_size*3]
+                elif "Tier 4" in rule_name and part_size > 0:
+                    rule['proxies'] = all_name[part_size*3:full_size]
 
+
+    # Обновление конфига
     config.update(all_provider_dic)
-    config.update({'proxy-groups': proxy_groups})
-    config.update({'proxies': proxy_all})
+    config['proxy-groups'] = proxy_groups
+    config['proxies'] = proxy_all
 
-    config_yaml = yaml.dump(config, default_flow_style=False, sort_keys=False,
-                            allow_unicode=True, width=750, indent=2, Dumper=NoAliasDumper)
 
-    Eternity_yml = open(output, 'w+', encoding='utf-8')
-    Eternity_yml.write(config_yaml)
-    Eternity_yml.close()
+    # Сохранение готового файла
+    config_yaml = yaml.dump(
+        config,
+        default_flow_style=False,
+        sort_keys=False,
+        allow_unicode=True,
+        width=750,
+        indent=2,
+        Dumper=NoAliasDumper
+    )
+
+    with open(output, 'w+', encoding='utf-8') as f:
+        f.write(config_yaml)
 
 
 if __name__ == '__main__':
-    sub_merge.geoip_update(
-        'https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb')
+    sub_merge.geoip_update('https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb')
     eternity_convert(Eterniy_file, config_file, output=Eternity_yml_file)
